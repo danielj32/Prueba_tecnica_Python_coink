731a7ed578abaaff038a3bd57c36e6bba8579704
danielj32 <44917805+danielj32@users.noreply.github.com>
2022-01-23 20:22:36 -0500

    add tasks

---
+++ app.py
@@ -0,0 +1,44 @@
+from asyncio import tasks
+from re import T
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/tasks.db'
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Nombre = db.Column(db.String(200))
+    Email = db.Column(db.String(200))
+    Ciudad = db.Column(db.String(200))
+    done = db.Column(db.Boolean)
+
+@app.route('/')
+def home():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/create-task' , methods=['POST'])
+def create():
+    task = Task(Nombre=request.form['Nombre'], Email=request.form['Email'], Ciudad=request.form['Ciudad'], done=False)
+    db.session.add(task)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/done/<id>')
+def done(id):
+    task = Task.query.filter_by(id=int(id)).first()
+    task.done = not(task.done)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/delete/<id>')
+def delete(id):
+    task = Task.query.filter_by(id=int(id)).delete()
+    db.session.commit()
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
